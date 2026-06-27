cf143f8bb97b704d9df24ee58cfd0b5e2b5ec69e
Peter <Vanderhell@gmail.com>
2026-06-27 19:11:43 +0200

    ci: suppress cppcheck slot const warning

--- src/num8lora_sender.c
+++ src/num8lora_sender.c
@@ -245,7 +245,7 @@ int num8lora_sender_register_receiver(num8lora_sender_t* s, uint16_t receiver_id
 
 int num8lora_sender_set_receiver_progress(num8lora_sender_t* s, uint16_t receiver_id, uint32_t last_acked_op_id)
 {
-    num8lora_sender_receiver_slot_t* slot;
+    num8lora_sender_receiver_slot_t* slot; /* cppcheck-suppress constVariablePointer */
     if (s == NULL)
     {
         return 0;
